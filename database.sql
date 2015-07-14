c2638e4eaaaae6c2f71e329ef1f008de19e54ca2
Dongdong Li (Andy) <673536904@qq.com>
2015-07-14 18:02:34 -0400

    Create database.sql
    These scripts create the table in the database.

---
+++ database.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE IF NOT EXISTS `upload_files` (
+`id` int(11) NOT NULL,
+  `fileName` varchar(100) NOT NULL,
+  `filePath` varchar(350) NOT NULL,
+  `fileDesc` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
